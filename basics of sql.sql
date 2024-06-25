0e2c29199f5dfb42538d2677427a86ed35b0a643
Siddhant1609 <siddhantsingh16092000@gmail.com>
2024-06-25 17:48:02 +0530

    SQL-Setup

---
+++ basics of sql.sql
@@ -0,0 +1,17 @@
+-- select*from film_n
+-- select title, description from film_n
+--select film_id as f_d, title as t, description as d
+-- from film_n
+-- select film_id, title, description
+-- from film_n
+-- limit 10
+
+
+-- select  film_id, title, description
+-- from film_n
+-- where film_id = 39
+
+select title, description
+from film_n
+where release_year = 2004 and rental_duration = 4
+limit 10
